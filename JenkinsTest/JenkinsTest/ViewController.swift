0d9658968487ecba3166cd557e13cf063b8af49a
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2021-09-20 17:38:55 +0200

    test jenkins with sample app

---
+++ JenkinsTest/JenkinsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JenkinsTest
+//
+//  Created by Srinivasan Rajendran on 2021-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
